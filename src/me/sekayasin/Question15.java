c225659fd7bc639591da02131a404fec9ea9265a
barahijawi <34750743+barahijawi@users.noreply.github.com>
2022-07-10 14:07:54 +0400

    Question15 Optimized Big(O)

--- src/me/sekayasin/Question15.java
+++ src/me/sekayasin/Question15.java
@@ -5,6 +5,22 @@ public class Question15 {
     public static void main(String[] args) {
         System.out.println(satisfiesConditions(new int[]{5,4,3,2,3,4,6,1}));
     }
+    //// Optimized solution
+        public static int answerThree(int[] a) {
+        int leftSum = 0;
+        int rightSum = 0;
+        if (a.length % 2 == 0) {
+            for (int i = 0; i < a.length; i++) {
+                if (i < a.length / 2)
+                    leftSum += a[i];
+                else rightSum += a[i];
+            }
+            if (rightSum == leftSum)
+                return 1;
+        }
+        return 0;
+    }
+    ////
 
     static int satisfiesConditions(int[] a) {
         int isSatisfied = 0;
